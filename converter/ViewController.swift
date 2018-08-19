08feaf520ad44ff6abe41d27212c03103e9a7d0b
christophemilliere <christophemilliere93@gmail.com>
2018-08-19 22:33:01 +0200

    finished

--- converter/ViewController.swift
+++ converter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ui_outputPouceLabel: UILabel!
+    @IBOutlet weak var ui_outputCentimeterLabel: UILabel!
+    @IBOutlet weak var ui_outputMeterLabel: UILabel!
+    @IBOutlet weak var inputValueField: UITextField!
+    @IBOutlet weak var ui_inputValueType: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func inputValueChanged() {
+        convertInputValue()
+    }
+    @IBAction func inputValueTypeChanged() {
+        convertInputValue()
+    }
+    
+    
+    func getInputMeterValue() -> Double? {
+        let inputMeters:Double?
+        
+        if let inputString: String = inputValueField.text, let inputDouble: Double = Double(inputString) {
+            switch ui_inputValueType.selectedSegmentIndex{
+            case 0:
+                inputMeters = inputDouble
+            case 1 :
+                inputMeters = UnitLength.centimeters.converter.baseUnitValue(fromValue: inputDouble)
+            case 2:
+                inputMeters = UnitLength.inches.converter.baseUnitValue(fromValue: inputDouble)
+            default:
+                inputMeters = nil
+            }
+        } else {
+            inputMeters = nil
+        }
+        
+        return inputMeters
+    }
+    func convertInputValue() {
+        if let inputMeter = getInputMeterValue() {
+            ui_outputMeterLabel.text = "\(inputMeter) M"
+            ui_outputPouceLabel.text = "\(UnitLength.inches.converter.baseUnitValue(fromValue: inputMeter)) pouces"
+            ui_outputCentimeterLabel.text = "\(UnitLength.centimeters.converter.baseUnitValue(fromValue: inputMeter)) cm"
+        } else {
+            ui_outputMeterLabel.text = ""
+            ui_outputPouceLabel.text = ""
+            ui_outputCentimeterLabel.text = ""
+        }
+    }
+    
 }
 
